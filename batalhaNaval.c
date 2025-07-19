fb715e8f5947c6533a12f888dea9143067693c6a
Nilton Pontes <niltoneapontes@gmail.com>
2025-07-19 15:08:23 +0000

    feat: nivel mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,23 +4,32 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+#define LINHAS 10
+#define COLUNAS 10
+
+#define LINHAS_HABILIDADE 3
+#define COLUNAS_HABILIDADE 5
+
+#define INDICADOR_MAR 0
+#define INDICADOR_NAVIO 3
+#define INDICADOR_AREA_AFETADA 5
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
 
     char letras[11] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-    int tabuleiro[10][10] = {
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    };
+    int tabuleiro[LINHAS][COLUNAS];
+
+    int cone[LINHAS_HABILIDADE][COLUNAS_HABILIDADE], cruz[LINHAS_HABILIDADE][COLUNAS_HABILIDADE], octaedro[LINHAS_HABILIDADE][COLUNAS_HABILIDADE];
+
+    int linha_inicio = 4, coluna_inicio = 4;
+
+    for(int i = 0; i < LINHAS; i++) {
+        for(int j = 0; j < COLUNAS; j++) {
+            tabuleiro[i][j] = INDICADOR_MAR; // Preenchendo os espacos do tabuleiro com 0;
+        }
+    }
 
     printf("*** TABULEIRO SEM NAVIOS ***\n\n");
 
@@ -30,9 +39,9 @@ int main() {
 
     printf("\n\n");
 
-    for(int i = 0; i <=9 ; i++) {
+    for(int i = 0; i <= COLUNAS - 1 ; i++) {
         printf("%d\t", i + 1);
-        for(int j = 0; j <= 9; j++) {
+        for(int j = 0; j <= COLUNAS - 1; j++) {
             printf("%d\t", tabuleiro[i][j]);
         }
         printf("\n");
@@ -45,29 +54,31 @@ int main() {
 
     // Posicionando os navios:
 
-    for(int i = 7; i <= 9; i++) { // Navio Horizontal: Linha 5 e colunas D, E, F
-        tabuleiro[6][i] = 3; // linha hardcoded
+    for(int i = 7; i <= COLUNAS - 1; i++) { // Navio Horizontal: Linha 5 e colunas D, E, F
+        tabuleiro[6][i] = INDICADOR_NAVIO; // linha hardcoded
     }
 
     for(int i = 3; i <= 5; i++) { // Navio Vertical: Coluna A e linhas 8 a 10
-        tabuleiro[i][0] = 3; // coluna hardcoded
+        tabuleiro[i][0] = INDICADOR_NAVIO; // coluna hardcoded
     }
 
     for(int i = 0; i <= 2; i++) { // Navio Diagonal Principal
         for(int j = 0; j <= 2; j++) {
             if(i == j) {
-                tabuleiro[i][j] = 3;
+                tabuleiro[i][j] = INDICADOR_NAVIO;
             }
         }
     }
 
-    for(int i = 7; i <= 9; i++) { // Navio Diagonal Secundaria
+    for(int i = 7; i <= COLUNAS - 1; i++) { // Navio Diagonal Secundaria
         for(int j = 0; j <= 2; j++) {
-            if(i + j == 9) {
-                tabuleiro[i][j] = 3;
+            if(i + j == COLUNAS - 1) {
+                tabuleiro[i][j] = INDICADOR_NAVIO;
             }
         }
     }
+
+    // Exibindo o tabuleiro
 
     for(int j = 0; j <= 10; j++) {
         printf("%c\t", letras[j]);
@@ -75,9 +86,9 @@ int main() {
 
     printf("\n\n");
 
-    for(int i = 0; i <=9 ; i++) {
+    for(int i = 0; i <= COLUNAS - 1 ; i++) {
         printf("%d\t", i + 1);
-        for(int j = 0; j <= 9; j++) {
+        for(int j = 0; j <= COLUNAS - 1; j++) {
             printf("%d\t", tabuleiro[i][j]);
         }
         printf("\n");
@@ -86,15 +97,112 @@ int main() {
     printf("\n\n");
 
 
+    printf("*** TABULEIRO COM NAVIOS E AREAS AFETADAS ***\n\n");
 
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Criando as areas afetadas:
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+
+    printf("\nCriando o cone...\n");
+
+    for (int i = 0; i < LINHAS_HABILIDADE; i++) {
+        for (int j = 0; j < LINHAS_HABILIDADE - i - 1; j++) {
+            cone[i][j] = INDICADOR_MAR;
+            printf("%d ", cone[i][j]);
+
+        }
+
+        for (int k = 0; k < 2 * i + 1; k++) {
+            cone[i][k] = INDICADOR_AREA_AFETADA;
+            printf("%d ", cone[i][k]);
+        }
+
+         for (int j = LINHAS_HABILIDADE + i; j < COLUNAS_HABILIDADE; j++) {
+            cone[i][j] = INDICADOR_MAR;
+            printf("%d ", cone[i][j]);
+
+        }
+
+        printf("\n");
+    }
+
+    printf("\nCriando a cruz...\n");
+
+    for (int i = 0; i < LINHAS_HABILIDADE; i++) {
+        for (int j = 0; j < COLUNAS_HABILIDADE; j++) {
+            if (i == LINHAS_HABILIDADE / 2 || j == COLUNAS_HABILIDADE / 2) {
+                cruz[i][j] = INDICADOR_AREA_AFETADA;
+                printf("%d ", cruz[i][j]);
+            } else {
+                cruz[i][j] = INDICADOR_MAR;
+                printf("%d ", cruz[i][j]);
+            }
+        }      
+
+        printf("\n");
+    }
+
+    printf("\nCriando o octaedro...\n");
+
+    for (int i = 0; i < LINHAS_HABILIDADE; i++) {
+        for (int j = 0; j < COLUNAS_HABILIDADE; j++) {
+            if (i == LINHAS_HABILIDADE / 2 || j == COLUNAS_HABILIDADE / 2) {
+                if(j == 0 || j == COLUNAS_HABILIDADE - 1) {
+                    octaedro[i][j] = INDICADOR_MAR;
+                    printf("%d ", octaedro[i][j]);
+                } else {
+                    octaedro[i][j] = INDICADOR_AREA_AFETADA;
+                    printf("%d ", octaedro[i][j]);
+                }
+            } else {
+                octaedro[i][j] = INDICADOR_MAR;
+                printf("%d ", octaedro[i][j]);
+            }
+        }      
+
+        printf("\n");
+    }
+
+    printf("\n\n");
+
+
+    // Sobrepondo as areas afetadas no tabuleiro
+
+    for(int i = 0; i < LINHAS_HABILIDADE; i++) {
+        int x = i + linha_inicio;
+        int centro_cone = coluna_inicio + COLUNAS_HABILIDADE / 2;
+        int inicio_cone = centro_cone - i;
+        int fim_cone = centro_cone + i;
+
+        for (int y = inicio_cone, j = 0; y <= fim_cone; j++, y++) {
+            tabuleiro[x][y] = cone[i][j];
+        }
+
+        // // Cruz ou Octaedro
+        // for(int j = 0; j < COLUNAS_HABILIDADE; j++) {
+        //     int x = i + linha_inicio;
+        //     int y = j + coluna_inicio;
+
+        //     if(x < LINHAS && y < COLUNAS) {
+        //         tabuleiro[x][y] = cruz[i][j];
+        //     }
+        // }
+    }
+
+    for(int j = 0; j <= 10; j++) {
+        printf("%c\t", letras[j]);
+    }
+
+    printf("\n\n");
+
+    for(int i = 0; i <= COLUNAS - 1 ; i++) {
+        printf("%d\t", i + 1);
+        for(int j = 0; j <= COLUNAS - 1; j++) {
+            printf("%d\t", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n");
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
